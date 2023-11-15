8e53da21b6fe9812f93605cf9812c79cbf6f85f8
Lucefer256 <sm30122000@gmail.com>
2023-11-15 23:42:39 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 
 import './App.css';
 import Nav from './Nav'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,Routes ,Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Nav/>
-      <h1>Home Page</h1>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>}></Route>
+        <Route path="/catagories" element={<h1>Categories</h1>}></Route>
+        <Route path="/addproduct" element={<h1>Add Products</h1>}></Route>
+        <Route path="/logout" element={<h1>Logout</h1>}></Route>
+        <Route path="/profile" element={<h1>Profile</h1>}></Route>
+      </Routes>
+      {/* <h1>Home Page</h1> */}
       </BrowserRouter>
     </div>
   );
--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const Nav=()=>{
-    return(
-        <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/app">APP</Link></li>
-        </ul>
+const Nav = () => {
+    return (
+        <div className="nav-ul">
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/catagories">Catagories</Link></li>
+                <li><Link to="/addproduct">Add Product</Link></li>
+                <li><Link to="/logout">Logout</Link></li>
+                <li><Link to="/profile">Profile</Link></li>
+
+            </ul>
+        </div>
     )
 }
 export default Nav;
