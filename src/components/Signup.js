e480d62a0fc0954f3da502d5723eff0666a156be
Lucefer256 <sm30122000@gmail.com>
2023-11-23 01:05:54 +0530

    modified appcss and signup compo

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
 const Signup =()=>{
+    const [name,setName]=useState("");
+    const [password,setPassword]=useState("");
+    const [email,setEmail]=useState("");
+    const collectData=()=>{
+        console.log(name,email,password)
+    }
+
     return(
         <div className="signup">
             <h1>SignUp</h1>
-            <input className="inputbox" type="text" placeholder="Enter Name"/>
-            <input className="inputbox" type="text" placeholder="Enter Email"/>
-            <input className="inputbox" type="text" placeholder="Enter password"/>
-            <button className="Signbutton" type="button">Sign UP</button>
+            <input className="inputbox" type="text" value={name} onChange={(e)=>setName(e.target.value)} placeholder="Enter Name"/>
+            <input className="inputbox" type="text" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Enter Email"/>
+            <input className="inputbox" type="text" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Enter password"/>
+            <button className="Signbutton" onClick={collectData} type="button">Sign UP</button>
         </div>
     )
 };
